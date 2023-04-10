afb75cc1dd6fd8a7dd3d7e7c878afbd1dd04c557
Curly228 <broitman.ie1998@gmail.com>
2023-04-10 19:43:50 +0300

    Add verify phishing framework

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
+import requests
+
 from urllib.parse import urlparse
 
+from urllib3.exceptions import ConnectTimeoutError
+
+
 def verifyTls(url):
-    result = "NO"
     if url.scheme == 'https':
-        result = "YES"
+        return "YES"
     elif url.scheme == 'http':
-        result = "NO"
-    return result
+        return "NO"
+    return "NO"
 
 
 def verifySubDomains(url):
     splitHost = url.hostname.split('.')
-    subDomain = splitHost[1] + "."+splitHost[2]+"\n"
+    subDomain = splitHost[1] + "." + splitHost[2] + "\n"
     file = open('static/top-1000000-domains', 'r')
     for line in file:
         if subDomain == line:
@@ -19,13 +23,26 @@ def verifySubDomains(url):
 
     return "NO"
 
+
+def verifyPhishingFramework(url):
+    result = "NO"
+    try:
+        requests.get(f'{url.scheme}://{url.hostname}:3333/', timeout=2)
+    except Exception as err:
+        result = "YES"
+    finally:
+        return result
+
+
 print('Введите сайт:\n')
-url = urlparse(input())
+# url = urlparse(input())
+url = urlparse("https://drive.google.com/drive/folders/1Y_0bynWBxCengUpb64ycYqlWqq5pyM2E")
 
 result = {"verifyTLS": verifyTls(url)}
 
-result.update( {"verifyDomain": verifySubDomains(url)})
+result.update({"verifyDomain": verifySubDomains(url)})
 
+result.update({"verifyPhishingFramework": verifyPhishingFramework(url)})
 
 print("Результат проверки сайта на фишинг ")
 print(result)
